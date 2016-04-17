3e9847f9ad9cc6f667310c3d68aa166033020e10
klimas7 <klimas7@gmail.com>
2016-04-17 16:17:14 +0200

    Add global config - numerical system

--- src/main/java/pl/klimas7/calculatorPlugin/CalculatorBuilder.java
+++ src/main/java/pl/klimas7/calculatorPlugin/CalculatorBuilder.java
@@ -8,12 +8,15 @@ import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractProject;
+import hudson.model.Descriptor;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
 import jenkins.tasks.SimpleBuildStep;
+import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest;
 
 public class CalculatorBuilder extends Builder implements SimpleBuildStep {
 
@@ -23,11 +26,23 @@ public class CalculatorBuilder extends Builder implements SimpleBuildStep {
 
     @Override
     public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath workspace, @Nonnull Launcher launcher, @Nonnull TaskListener listener) throws InterruptedException, IOException {
+        listener.getLogger().println("Numerical system is: " + getDescriptor().getNumericalSystem());
         listener.getLogger().println("Calculation result:");
+    }
+
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
     }
 
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        private NumericalSystem numericalSystem;
+
+        public DescriptorImpl() {
+            load();
+        }
 
         @Override
         public boolean isApplicable(Class<? extends AbstractProject> jobType) {
@@ -38,6 +53,18 @@ public class CalculatorBuilder extends Builder implements SimpleBuildStep {
         @Override
         public String getDisplayName() {
             return "Calculate";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+
+            numericalSystem = NumericalSystem.valueOf((String) json.get("numericalSystem"));
+            save();
+            return super.configure(req, json);
+        }
+
+        public NumericalSystem getNumericalSystem() {
+            return numericalSystem;
         }
     }
 }
---
+++ src/main/java/pl/klimas7/calculatorPlugin/NumericalSystem.java
@@ -0,0 +1,6 @@
+package pl.klimas7.calculatorPlugin;
+
+
+public enum NumericalSystem {
+    HEX, OCT, BIN, DEC
+}
